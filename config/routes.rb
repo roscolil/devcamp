9d24d2bc56239551d535e30d33828f4212e9913d
Ross Lillis <ross.lillis@gmail.com>
2019-04-04 10:16:18 +1100

    updated status for enum

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   
   resources :portfolios , except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
   resources :blogs
-  
+
   root to: 'pages#home'
+  
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
-
 end
