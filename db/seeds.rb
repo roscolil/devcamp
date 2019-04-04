727ca7ce03cf897a290e8136ae57c12bb7d8eaa6
Ross Lillis <ross.lillis@gmail.com>
2019-04-04 14:47:39 +1100

    Integrated tech model

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,3 +57,10 @@ end
 
 puts "1 portfolio items created"
 
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology: #{technology}"
+  )
+end
+
+puts "3 technologies created"
